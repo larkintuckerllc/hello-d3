56a2cbcd1081cf068a14196b337686573f890d4b
John  Tucker <john@larkintuckerllc.com>
2017-09-22 16:00:44 -0400

    Refactor with another example

--- selections/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var d3 = window.d3;
-document.addEventListener('DOMContentLoaded', function() {
-  var listD3 = d3.select('#list').style('background-color', 'black');
-  listD3.selectAll('li').style('color', 'white');
-});
