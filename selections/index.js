75418da7cdb7b391dfe1de12c7ea899eae2918a0
John  Tucker <john@larkintuckerllc.com>
2017-09-22 12:21:29 -0400

    First commit

---
+++ selections/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var d3 = window.d3;
+document.addEventListener('DOMContentLoaded', function() {
+  d3.select('body').style('background-color', 'black');
+  d3.selectAll('p').style('color', 'white');
+});
