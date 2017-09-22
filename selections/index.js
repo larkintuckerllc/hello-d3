794314919b05fae492e6a3ac2c48edb1ce64531e
John  Tucker <john@larkintuckerllc.com>
2017-09-22 12:43:55 -0400

    Use UL and LI

--- selections/index.js
+++ selections/index.js
@@ -1,6 +1,6 @@
 "use strict";
 var d3 = window.d3;
 document.addEventListener('DOMContentLoaded', function() {
-  d3.select('body').style('background-color', 'black');
-  d3.selectAll('p').style('color', 'white');
+  var listD3 = d3.select('#list').style('background-color', 'black');
+  listD3.selectAll('li').style('color', 'white');
 });
