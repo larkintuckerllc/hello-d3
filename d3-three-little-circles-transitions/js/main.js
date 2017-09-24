eadb1d5448da43d858f804d076a780aecc7ac327
John  Tucker <john@larkintuckerllc.com>
2017-09-24 07:14:00 -0400

    circle animation

---
+++ d3-three-little-circles-transitions/js/main.js
@@ -0,0 +1,17 @@
+const DATASET = [32, 57, 293];
+var domReady = function(callback) {
+  document.readyState === 'interactive' || document.readyState === 'complete' ? callback() : document.addEventListener('DOMContentLoaded', callback);
+};
+domReady(function() {
+  var selection = d3.select('#chart')
+    .selectAll('.circle')
+    .data(DATASET);
+  selection.enter()
+    .append('circle')
+    .attr('class', 'circle')
+    .attr('cy', 60)
+    .attr('cx', function(d, i) { return i * 100 + 30; })
+    .attr('r', function(d) { return Math.sqrt(d); });
+  // ADDED TRANSITION
+  selection.transition().attr('r', function(d) { return (2 * Math.sqrt(d)); }).duration(2000);
+});
